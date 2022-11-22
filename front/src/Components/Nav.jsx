e70c5c9f8c3ebfa3e4ac3b7a50f1134d90742ba3
sjurate <rasaujuratei@gmail.com>
2022-11-22 17:19:34 +0200

    logo hoHome

--- front/src/Components/Nav.jsx
+++ front/src/Components/Nav.jsx
@@ -1,16 +1,24 @@
 import { useContext } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import DataContext from "../Contexts/DataContext";
 
 function Nav({ status }) {
   const { showLinks, setShowLinks } = useContext(DataContext);
+
+  const navigate = useNavigate();
+
+  const goHome = () => {
+    navigate("/", { replace: true });
+  };
 
   return (
     <div className="container">
       <div className="row">
         <nav className="navbar navbar-expand-md navbar-dark bg-dark col-12">
           <div className="container-fluid">
-            <span className="navbar-brand">GoFundMe</span>
+            <span className="navbar-brand click-link" onClick={goHome}>
+              GoFundMe
+            </span>
             <div>
               <div className="navbar-nav" id={showLinks ? "hidden" : ""}>
                 {status === 2 || status === 3 || status === 4 ? (
